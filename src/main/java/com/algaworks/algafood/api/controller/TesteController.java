bced5a38d577760079d5b10a99dc34dbb974e9e4
Alextony Rodrigues <alextonyrodrigues@gmail.com>
2022-04-19 12:29:55 -0300

    implementando uma consulta jpql dinamica

--- src/main/java/com/algaworks/algafood/api/controller/TesteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/TesteController.java
@@ -34,9 +34,14 @@ public class TesteController {
 		return restauranteRepository.findByTaxaFreteBetween(taxaInicial, taxaFinal);
 	}
 	
+	@GetMapping("/restaurantes/por-nome-e-frete")
+	public List<Restaurante> restaurantesPorNomeFrete(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal){
+		return restauranteRepository.find(nome, taxaFreteInicial, taxaFreteFinal);
+	}
+	
 	@GetMapping("/restaurantes/por-nome-id")
 	public List<Restaurante> restaurantePorNomeId(String nome, Long id){
-		return restauranteRepository.findByNomeContainingAndCozinhaId(nome, id);
+		return restauranteRepository.consultarPorNome(nome, id);
 	}
 	
 	@GetMapping("/restaurantes/primeiro-por-nome")
