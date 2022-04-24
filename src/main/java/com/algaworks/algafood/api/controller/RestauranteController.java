eb6104aea7defceef7f6646bb0b2bd988c55de09
Alextony Rodrigues <alextonyrodrigues@gmail.com>
2022-04-24 16:53:22 -0300

    Mapeando propriedades com @CreationTimestamp e @UpdateTimestamp

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -65,7 +65,7 @@ public class RestauranteController {
 			
 			Restaurante restauranteAtual = cadastroRestauranteService.buscar(id);
 			if (restauranteAtual != null) {
-				BeanUtils.copyProperties(restaurante, restauranteAtual, "id", "pagamento", "endereco");
+				BeanUtils.copyProperties(restaurante, restauranteAtual, "id", "pagamento", "endereco", "dataCadastro");
 			Restaurante	restauranteSalva = cadastroRestauranteService.salvar(restauranteAtual);
 				return ResponseEntity.ok(restauranteSalva);
 
--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.domain.model;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import javax.persistence.Column;
@@ -14,6 +15,9 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -44,6 +48,16 @@ public class Restaurante {
 	@JsonIgnore
 	@Embedded
 	private Endereco endereco;
+	
+	@JsonIgnore
+	@CreationTimestamp
+	@Column(nullable = false, columnDefinition = "datetime")
+	private LocalDateTime dataCadastro;
+	
+	@JsonIgnore
+	@UpdateTimestamp
+	@Column(nullable = false, columnDefinition = "datetime")
+	private LocalDateTime dataAtualizacao;
 	
 	
 	@JsonIgnore
