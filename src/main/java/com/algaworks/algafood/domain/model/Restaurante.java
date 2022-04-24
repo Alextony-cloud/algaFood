2b0db7d0fe79224dfb9ab69cd7b2770eb1b72ad7
Alextony Rodrigues <alextonyrodrigues@gmail.com>
2022-04-24 15:08:26 -0300

    Mapeando classes incorporáveis com @Embedded e @Embeddable

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -36,6 +37,9 @@ public class Restaurante {
 	@ManyToOne
 	@JoinColumn(name="cozinha_id", nullable = false)
 	private Cozinha cozinha;
+	
+	@Embedded
+	private Endereco endereco;
 	
 	@Column(name="taxa_frete", nullable = false)
 	private BigDecimal taxaFrete;
